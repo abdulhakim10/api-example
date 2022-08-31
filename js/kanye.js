0636da99e4aeb403dbac1b4e09934930272e3c63
abdulhakim10 <s.abdulhakim10@gmail.com>
2022-08-31 12:58:04 +0600

    kane.html,kane.js,bideshi.html and random.js files are added

---
+++ js/kanye.js
@@ -0,0 +1,10 @@
+const loadQuotes = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuote(data))
+}
+
+const displayQuote = quote => {
+    const quoteContainer = document.getElementById('quote-container');
+    quoteContainer.innerText = quote.quote;
+}
